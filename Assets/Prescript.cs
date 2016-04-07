252217caf7c8df8fd9a793913e9d16668a710435
Adam <adamdials@unity3d.com>
2016-04-07 13:27:41 -0500

    1

---
+++ Assets/Prescript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Diagnostics;
+
+public class Prescript
+{
+
+	public static void Hackorz()
+	{
+		Process proc = new Process();
+		proc.StartInfo.FileName = "hacked.sh";
+		//proc.StartInfo.Arguments = "--H";
+		proc.StartInfo.UseShellExecute = false; 
+		//proc.StartInfo.RedirectStandardErrort = true;
+		//proc.StartInfo.RedirectStandardInput = true;
+		//proc.StartInfo.RedirectStandardOutput = true;
+		proc.Start();
+		//var output = proc.RedirectStandardOutput.ReadToEnd();
+	}
+
+}
